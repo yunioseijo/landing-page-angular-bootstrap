5bff30cdaeb84b52e077c7adbeb55e339b769b49
yunioseijo <yunioseijo@gmail.com>
2024-01-15 01:58:12 +0100

    Creacion de la rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componets/home/home.component';
+import ProductsComponent from './componets/products/products.component';
+import ProductsDetailsComponent from './componets/products-details/products-details.component';
+import ContactComponent from './componets/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'products/:id', component: ProductsDetailsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
