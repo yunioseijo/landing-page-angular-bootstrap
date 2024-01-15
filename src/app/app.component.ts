e315b5d3dfbfbdf066c99dbd666e0ad311f7fc3f
yunioseijo <yunioseijo@gmail.com>
2024-01-15 01:59:47 +0100

    Creacion de la navegacion con routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],// es muy importante la importación de RouterLink para que pueda navegar
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'landing-page-angular-bootstrap';
+
+  menuOption: string = ''
+  
+  onOption(menuOption: string){
+    this.menuOption = menuOption
+  }
 }
