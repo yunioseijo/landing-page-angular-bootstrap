b2f1a28d5f32731b43b8f63739c0e29dfe9e6785
yunioseijo <yunioseijo@gmail.com>
2024-01-15 02:49:00 +0100

    Pasando varios parametros por la url y agreganso estilos dinámicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import ContactComponent from './componets/contact/contact.component';
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'products', component: ProductsComponent },
-    { path: 'products/:id', component: ProductsDetailsComponent },
+    { path: 'products/:id/:category', component: ProductsDetailsComponent },
     { path: 'contact', component: ContactComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/componets/products-details/products-details.component.ts
+++ src/app/componets/products-details/products-details.component.ts
@@ -1,16 +1,18 @@
+import { NgStyle } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products-details',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './products-details.component.html',
   styleUrl: './products-details.component.css'
 })
 export default class ProductsDetailsComponent implements OnInit {
 
   producto: string = '';
+  category: string = '';
   _route = inject(ActivatedRoute);
 
 
@@ -18,6 +20,7 @@ export default class ProductsDetailsComponent implements OnInit {
     this._route.params.subscribe(params => {
       console.log('Nombre del producto',params);
       this.producto = params['id'];
+      this.category = params['category'];
 
       });
     }
