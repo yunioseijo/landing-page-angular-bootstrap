ccf1a832ab342cbaf7a56bcbefc92777ef097682
yunioseijo <yunioseijo@gmail.com>
2024-01-15 12:31:23 +0100

    creo un mock de products con su interface product

--- src/app/componets/products/products.component.ts
+++ src/app/componets/products/products.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { productsList } from './products.mock';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],// Para usar el pipe currency hay que importar commonmodule
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export default class ProductsComponent {
+  productsList = productsList;
   
   }
---
+++ src/app/componets/products/products.mock.ts
@@ -0,0 +1,12 @@
+export const productsList:Product[] = [
+    {id: 1, name: 'Lavandina', price: 10, description: 'Botella de un litro'},
+    {id: 2, name: 'Detergente', price: 5, description: 'Dura 45 lavados'},
+    {id: 3, name: 'Leche Condensada', price: 3.89, description: 'El sabor de tu preferencia'},
+    {id: 4, name: 'Arroz', price: 1.3, description: 'Un kg de arroz balnco largo'},
+]
+export interface Product{
+    id: number;
+    name: string;
+    price: number;
+    description:  string;
+}
